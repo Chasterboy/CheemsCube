565b8cc3edd2a3f0402b0e75da8938ef74be1f44
Juan Eduardo Rivera Perez <68937183+Chasterboy@users.noreply.github.com>
2023-04-15 23:59:32 -0600

    cube

--- Assets/Scripts/CubeMove.cs
+++ Assets/Scripts/CubeMove.cs
@@ -9,26 +9,35 @@ public class CubeMove : MonoBehaviour
     Vector2 secondPos;
     Vector2 currentSwipe;
 
+    float speed = 200f;
+
     float rotationSpeed = 200f;
+    Vector3 MousePsition;
+    Vector3 mousedelta;
 
     // Update is called once per frame
     void Update()
     {
         Swipe();
-        RotateTowardsTarget();
+        if (transform.rotation != target.transform.rotation)
+        {
+            var step = speed * Time.deltaTime;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, target.transform.rotation, step);
+        }
     }
 
     void Swipe()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            firstPressPos = Input.mousePosition;
+            firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         }
 
         if (Input.GetMouseButtonUp(0))
         {
-            secondPos = Input.mousePosition;
-            currentSwipe = (secondPos - firstPressPos).normalized;
+            secondPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            currentSwipe = new Vector2(secondPos.x - firstPressPos.x, secondPos.y - firstPressPos.y);
+            currentSwipe.Normalize();
 
             if (LeftSwipe(currentSwipe))
             {
@@ -38,27 +47,56 @@ public class CubeMove : MonoBehaviour
             {
                 target.transform.Rotate(0, -90, 0, Space.World);
             }
+            else if (UpLeftSwipe(currentSwipe))
+            {
+                target.transform.Rotate(0, 0, 90, Space.World);
+            }
+            else if (UpRightSwipe(currentSwipe))
+            {
+                target.transform.Rotate(90, 0, 0, Space.World);
+            }
+            else if (DownLeftSwipe(currentSwipe))
+            {
+                target.transform.Rotate(-90, 0, 0, Space.World);
+            }
+            else if (DownRightSwipe(currentSwipe))
+            {
+                target.transform.Rotate(0, 0, -90, Space.World);
+
+            }
+
         }
     }
 
     bool LeftSwipe(Vector2 swipe)
     {
-        return swipe.x < 0 && swipe.y > -0.5f && swipe.y < 0.5f;
+        return currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f;
     }
 
     bool RightSwipe(Vector2 swipe)
     {
-        return swipe.x > 0 && swipe.y > -0.5f && swipe.y < 0.5f;
+        return currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f;
     }
 
-    void RotateTowardsTarget()
+    bool UpLeftSwipe(Vector2 swipe)
     {
-        if (transform.rotation == target.transform.rotation)
-        {
-            return;
-        }
-
-        var step = rotationSpeed * Time.deltaTime;
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, target.transform.rotation, step);
+        return currentSwipe.y > 0 && currentSwipe.x < 0f;
     }
+    bool UpRightSwipe(Vector2 swiper)
+    {
+
+        return currentSwipe.y > 0 && currentSwipe.x > 0f;
+
+    }
+    bool DownLeftSwipe(Vector2 swiper)
+    {
+
+        return currentSwipe.y < 0 && currentSwipe.x < 0f;
+    }
+    bool DownRightSwipe(Vector2 swiper)
+    {
+        return currentSwipe.y < 0 && currentSwipe.x > 0f;
+    }
+
+
 }
